76aa9752c54a153f41a32d5e1f903a44c0ec9881
sajit <sajit@kunnumkal.com>
2025-01-25 21:11:43 -0500

    compiling Signed-off-by: Sajit Kunnumkal <sajit.kunnumkal@gmail.com>
    Signed-off-by: sajit <sajit@kunnumkal.com>

--- pkg/plugins/snowflake/cmd/main/main.go
+++ pkg/plugins/snowflake/cmd/main/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"time"
 
+	"github.com/hashicorp/go-plugin"
 	commonconfig "github.com/opencost/opencost-plugins/common/config"
 	commonrequest "github.com/opencost/opencost-plugins/common/request"
 	snowflakeconfig "github.com/opencost/opencost-plugins/pkg/plugins/snowflake/config"
@@ -11,7 +13,19 @@ import (
 	"github.com/opencost/opencost/core/pkg/log"
 	"github.com/opencost/opencost/core/pkg/model/pb"
 	"github.com/opencost/opencost/core/pkg/opencost"
+	ocplugin "github.com/opencost/opencost/core/pkg/plugin"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
+
+// handshakeConfigs are used to just do a basic handshake between
+// a plugin and host. If the handshake fails, a user friendly error is shown.
+// This prevents users from executing bad plugins or executing a plugin
+// directory. It is a UX feature, not a security feature.
+var handshakeConfig = plugin.HandshakeConfig{
+	ProtocolVersion:  1,
+	MagicCookieKey:   "PLUGIN_NAME",
+	MagicCookieValue: "snowflake",
+}
 
 // SnowflakeClient defines the interface for interacting with Snowflake
 type SnowflakeClient interface {
@@ -24,6 +38,17 @@ type snowflakeClient struct {
 }
 
 // NewSnowflakeClient creates and returns a new SnowflakeClient instance
+// NewSnowflakeClient creates a new Snowflake client using the provided Snowflake configuration.
+// It constructs a DSN (Data Source Name) string from the configuration and attempts to open a connection to Snowflake.
+// If the connection is successful and the database is reachable, it returns a SnowflakeClient instance.
+// If there is an error during the connection or ping process, it returns an error.
+//
+// Parameters:
+//   - snowflakeConfig: A pointer to a SnowflakeConfig struct containing the necessary configuration details.
+//
+// Returns:
+//   - SnowflakeClient: An instance of the SnowflakeClient interface if the connection is successful.
+//   - error: An error if there is an issue with the connection or ping process.
 func NewSnowflakeClient(snowflakeConfig *snowflakeconfig.SnowflakeConfig) (SnowflakeClient, error) {
 	dsn := fmt.Sprintf("user=%s password=%s account=%s db=%s schema=%s warehouse=%s",
 		snowflakeConfig.Username,
@@ -58,32 +83,48 @@ type SnowflakeCostSource struct {
 
 // GetInvoices fetches invoices from Snowflake
 func GetInvoices(snowflakeClient SnowflakeClient) ([]snowflakeplugin.LineItem, error) {
-	// Example query
-	//TODO make sure that query maps to snowflakeplugin.LineItem
-	query := `
-		SELECT 
-			date_trunc('day', start_time) AS usage_date,
-			SUM(credits_used) AS total_credits
-		FROM snowflake.account_usage.warehouse_metering_history
-		WHERE start_time >= DATEADD(day, -30, CURRENT_TIMESTAMP())
-		GROUP BY usage_date
-		ORDER BY usage_date DESC
-	`
+
+	query := snowflakeplugin.CreditsByWarehouse()
 
 	// Execute the query using the Snowflake client
 	rows, err := snowflakeClient.ExecuteQuery(query)
 	if err != nil {
-		log.Fatal("Query execution failed:", err)
+
+		log.Fatalf("Query execution failed:", err)
 		return nil, err
 	}
 	defer rows.Close()
-	// Implement the logic to fetch pending invoices from Snowflake
-	// This is a placeholder implementation
-	return rows, nil
+
+	var lineItems []snowflakeplugin.LineItem
+
+	for rows.Next() {
+		var warehouse string
+		var credits float32
+		var date string
+
+		if err := rows.Scan(&date, &warehouse, &credits); err != nil {
+			log.Fatalf("", err)
+		}
+
+		lineItem := snowflakeplugin.LineItem{
+			WarehouseName: warehouse,
+			CreditUsed:    credits,
+			Date:          date,
+		}
+
+		lineItems = append(lineItems, lineItem)
+	}
+
+	if err = rows.Err(); err != nil {
+		log.Fatalf("", err)
+	}
+
+	return lineItems, nil
+
 }
 
 func main() {
-	
+
 	log.Debug("Initializing Snowflake plugin")
 
 	configFile, err := commonconfig.GetConfigFilePath()
@@ -102,12 +143,12 @@ func main() {
 	}
 
 	client, err := NewSnowflakeClient(snowflakeConfig)
-	
+
 	if err != nil {
-		log.Fatal("Failed to create Snowflake client:", err)
+		log.Fatalf("Failed to create Snowflake client:", err)
 	}
 	snowflakeCostSource := SnowflakeCostSource{
-		snowflakeClient: client
+		snowflakeClient: client,
 	}
 	defer client.(*snowflakeClient).db.Close()
 
@@ -122,11 +163,9 @@ func main() {
 		GRPCServer:      plugin.DefaultGRPCServer,
 	})
 
-	
-
-	
 }
 func (s *SnowflakeCostSource) GetCustomCosts(req *pb.CustomCostRequest) []*pb.CustomCostResponse {
+
 	results := []*pb.CustomCostResponse{}
 
 	requestErrors := commonrequest.ValidateRequest(req)
@@ -164,31 +203,42 @@ func (s *SnowflakeCostSource) GetCustomCosts(req *pb.CustomCostRequest) []*pb.Cu
 		}
 
 		log.Debugf("fetching atlas costs for window %v", target)
-
-		// Print the results
-	fmt.Println("Date\t\tTotal Credits")
-	for rows.Next() {
-		var date string
-		var credits float64
-		var warehouse string
-		if err := rows.Scan(&date, &credits, &warehouse); err != nil {
-			log.Fatal(err)
-		}
-		//TODO extract lineItem into CustomCostResponse
-		//result := a.getAtlasCostsForWindow(&target, lineItems)
+		result := s.GetSnowflakeCostsForWindow(&target, lineItems)
 
 		results = append(results, result)
-		//fmt.Printf("%s\t%.2f\n", date, credits)
-
-	}
-
-	// Check for errors from iterating over rows
-	if err = rows.Err(); err != nil {
-		log.Fatal(err)
-	}
 
 	}
 
 	return results
 
+}
+func filterLineItemsByWindow(win *opencost.Window, lineItems []snowflakeplugin.LineItem) []*pb.CustomCost {
+	var filteredItems []*pb.CustomCost
+	for _, li := range lineItems {
+		if li.Date == win.Start().Format("2006-01-02 15:04:05") {
+			cost := &pb.CustomCost{
+				UsageQuantity: li.CreditUsed,
+				ResourceName:  li.WarehouseName,
+			}
+			filteredItems = append(filteredItems, cost)
+		}
+	}
+	return filteredItems
+}
+
+func (s *SnowflakeCostSource) GetSnowflakeCostsForWindow(win *opencost.Window, lineItems []snowflakeplugin.LineItem) *pb.CustomCostResponse {
+
+	costsInWindow := filterLineItemsByWindow(win, lineItems)
+	resp := pb.CustomCostResponse{
+		Metadata:   map[string]string{"api_client_version": "v1"},
+		CostSource: "data_storage",
+		Domain:     "snowflake",
+		Version:    "v1",
+		Currency:   "USD",
+		Start:      timestamppb.New(*win.Start()),
+		End:        timestamppb.New(*win.End()),
+		Errors:     []string{},
+		Costs:      costsInWindow,
+	}
+	return &resp
 }
--- pkg/plugins/snowflake/go.mod
+++ pkg/plugins/snowflake/go.mod
@@ -1,22 +1,41 @@
 module github.com/opencost/opencost-plugins/pkg/plugins/snowflake
 
-go 1.22.5
+go 1.22.7
 
 replace github.com/opencost/opencost-plugins/common => ../../common
 
 require (
+	github.com/hashicorp/go-plugin v1.6.1
 	github.com/opencost/opencost-plugins/common v0.0.0-00010101000000-000000000000
-	github.com/opencost/opencost/core v0.0.0-20240829194822-b82370afd830
+	github.com/opencost/opencost/core v0.0.0-20250117200701-47c4b6817505
+	google.golang.org/protobuf v1.33.0
 
 )
 
 require (
+	github.com/fatih/color v1.16.0 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/go-logr/logr v1.2.4 // indirect
+	github.com/goccy/go-json v0.9.11 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/gofuzz v1.2.0 // indirect
+	github.com/hashicorp/errwrap v1.0.0 // indirect
+	github.com/hashicorp/go-hclog v1.6.2 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/hashicorp/yamux v0.1.1 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/kr/pretty v0.3.1 // indirect
 	github.com/magiconair/properties v1.8.5 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mitchellh/go-testing-interface v1.14.1 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/oklog/run v1.1.0 // indirect
+	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
 	github.com/pelletier/go-toml v1.9.3 // indirect
 	github.com/rs/zerolog v1.26.1 // indirect
 	github.com/spf13/afero v1.6.0 // indirect
@@ -26,13 +45,20 @@ require (
 	github.com/spf13/viper v1.8.1 // indirect
 	github.com/stretchr/testify v1.9.0 // indirect
 	github.com/subosito/gotenv v1.2.0 // indirect
-	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/exp v0.0.0-20221031165847-c99f073a8326 // indirect
+	golang.org/x/net v0.23.0 // indirect
 	golang.org/x/sys v0.24.0 // indirect
 	golang.org/x/text v0.17.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240221002015-b0ce06bbee7c // indirect
 	google.golang.org/grpc v1.62.0 // indirect
-	google.golang.org/protobuf v1.32.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	k8s.io/api v0.25.3 // indirect
+	k8s.io/apimachinery v0.25.3 // indirect
+	k8s.io/klog/v2 v2.80.0 // indirect
+	k8s.io/utils v0.0.0-20220728103510-ee6ede2d64ed // indirect
+	sigs.k8s.io/json v0.0.0-20220713155537-f223a00ba0e2 // indirect
+	sigs.k8s.io/structured-merge-diff/v4 v4.2.3 // indirect
 )
